522379b29a2124ba89d12dddc585683e2660becc
Daniel Valdes <danielvaldes.dev@gmail.com>
2024-12-25 23:05:54 -0500

    Renombrar endpoint de tareas y agregar endpoint para agregar una nueva tarea

--- projectEF/Program.cs
+++ projectEF/Program.cs
@@ -18,10 +18,22 @@ app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
     return Results.Ok($"Base de datos en memoria: {dbContext.Database.IsInMemory()}");
 });
 
-app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext) => 
+app.MapGet("/api/consultarTareas", async ([FromServices] TareasContext dbContext) => 
 {
     var tareas = await dbContext.Tareas.Include(t => t.Categoria).ToListAsync();
     return Results.Ok(tareas);
+});
+
+app.MapPost("/api/agregarTarea", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea) => 
+{
+    tarea.TareaId = Guid.NewGuid();
+    tarea.FechaCreacion = DateTime.Now;
+    await dbContext.AddAsync(tarea); // Opción 1 para agregar un registro
+    // await dbContext.Tareas.AddAsync(tarea); // Opción 2 para agregar un registro
+
+    await dbContext.SaveChangesAsync();
+
+    return Results.Ok();
 });
 
 app.Run();
