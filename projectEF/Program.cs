907bb4d363ca26f09e451219665956ec63da42a6
Daniel Valdes <danielvaldes.dev@gmail.com>
2024-12-25 23:30:51 -0500

    Agregar endpoints para actualizar y eliminar tareas

--- projectEF/Program.cs
+++ projectEF/Program.cs
@@ -36,4 +36,37 @@ app.MapPost("/api/agregarTarea", async ([FromServices] TareasContext dbContext, 
     return Results.Ok();
 });
 
+app.MapPut("/api/actualizarTarea/{id}", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea, [FromRoute] Guid id) => 
+{
+    var tareaActual = dbContext.Tareas.Find(id);
+
+    if (tareaActual != null){
+        tareaActual.CategoriaId = tarea.CategoriaId;
+        tareaActual.Titulo = tarea.Titulo;
+        tareaActual.Descripcion = tarea.Descripcion;
+        tareaActual.PrioridadTarea = tarea.PrioridadTarea;
+
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
+});
+
+app.MapDelete("/api/eliminarTarea/{id}", async ([FromServices] TareasContext dbContext, [FromRoute] Guid id) => 
+{
+    var tareaActual = dbContext.Tareas.Find(id);
+
+    if (tareaActual != null){
+        dbContext.Remove(tareaActual);
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok($"Tarea con titulo: {tareaActual.Titulo}, se elimino exitosamente");
+    }
+
+    return Results.NotFound("Tarea no encontrada");
+});
+
+
 app.Run();
