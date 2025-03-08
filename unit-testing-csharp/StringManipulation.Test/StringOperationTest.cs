bd3cf475bf32322bb88924a789347dadf686572f
Daniel Valdes <danielvaldes.dev@gmail.com>
2025-03-08 18:38:16 -0500

    Proyecto de prueba

---
+++ unit-testing-csharp/StringManipulation.Test/StringOperationTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StringManipulation.Test
+{
+    public class StringOperationTest
+    {
+        [Fact]
+        public void ConcatenateStrings()
+        {
+            var strOperation = new StringOperations();
+            var result = strOperation.ConcatenateStrings("Hello", "World");
+            Assert.Equal("Hello World", result);
+        }
+    }
+}
